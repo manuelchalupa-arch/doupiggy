82ae80112474b394a99431a61505a0a914f91876
juan manuel perez <manuelchalupa@gmail.com>
2026-08-25 10:11:46 -0300

    Add files via upload

--- src/utils/format.js
+++ src/utils/format.js
@@ -2,6 +2,6 @@
 export const formatoARS = new Intl.NumberFormat("es-AR", {
   style: "currency",
   currency: "ARS",
-  minimumFractionDigits: 0,
-  maximumFractionDigits: 0,
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
 });
